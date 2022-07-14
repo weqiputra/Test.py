c0068cf544e344ec650ba953487c98f2af3d848e
We99on <rehat02belas@gmail.com>
2022-07-14 11:09:10 +0800

    test.py

---
+++ main.py
@@ -0,0 +1,17 @@
+a=10
+b=5
+panjang =1000
+print(" nilai a =", a)
+print(" nilai b =", b)
+print(" nilai panjang =", panjang)
+
+#penamanaan
+nilai_y=0
+nilaiz=3
+
+
+#tipe data khusus 
+#bilsnhsn kompleks
+data_complex = complex(5,6)
+print('data =', data_complex)
+print("tipe data =", type(data_complex))
